867652065d3d9c1cce2a828679f33348b57a7b84
Aliaksandr Kruk <Aliaksandr_Kruk@epam.com>
2018-11-16 16:46:28 +0300

    remove input and output directories

--- task3.js
+++ task3.js
@@ -6,22 +6,23 @@ let inputDir = "D:/work/Javascript/JSU1T3/input/";
 let outputDir = "D:/work/Javascript/JSU1T3/output/";
 let fs = require('fs');
 let path = require('path');
+let file,fileName;
 let walk = function(dir, done) {
   fs.readdir(dir, function(err, list) {
     if (err) return done(err);
-    var pending = list.length;
-    if (!pending) return done(null, results);
+    let pending = list.length;
+    if (!pending) return done(null);
     list.forEach(function(file) {
       fileName = file.replace(".json","");
       file = path.resolve(dir, file);
       fs.stat(file, function(err, stat) {
         if (stat && stat.isDirectory()) {
           walk(file, function(err, res) {
-            if (!--pending) done(null, results);
+            if (!--pending) done(null);
           });
         } else {
           convert(file,fileName);
-          if (!--pending) done(null, results);
+          if (!--pending) done(null);
         }
       });
     });
@@ -32,13 +33,25 @@ function convert(file,fileName){
   let wb = XLSX.utils.book_new();
   let ws_data = [[]];
   console.log(Object.entries(jsonObject));
-  Object.entries(jsonObject).forEach(([key, value]) => {let item = [[key,value]];
+  Object.entries(jsonObject).forEach(([key, value]) => {
+    let item = [[key,value]];
+    if(Array.isArray(item[1])){
+      item[1] = item[1].toString;
+      // let stringForArrayItems = "[";
+      // forEach(valueItem in value)
+      // console.log(valueItem);
+      // {
+      //   stringForArrayItems = stringForArrayItems.concat(",",value);
+      // }
+      // stringForArrayItems = stringForArrayItems.concat("","]");
+      // item[1] = stringForArrayItems;
+    } 
   ws_data = ws_data.concat(item);})
   let ws = XLSX.utils.aoa_to_sheet(ws_data);
   XLSX.utils.book_append_sheet(wb, ws, new_ws_name);
   if(typeof require !== 'undefined') XLSX = require('xlsx');
   XLSX.writeFile(wb,outputDir + fileName + ".xlsx");
 }
-walk(inputDir, function(err, results) {
+walk(inputDir, function(err) {
   if (err) throw err;
 });
