e8a5485cae8fbb8a3aa47eb204e4506c7ef4e806
Aliaksandr Kruk <Aliaksandr_Kruk@epam.com>
2018-11-19 16:22:39 +0300

    final version

--- task3.js
+++ task3.js
@@ -3,10 +3,10 @@
 const XLSX = require("xlsx");
 const fs = require("fs");
 
-const new_ws_name = "Sheet1";
+const whiteSheet_name = "Sheet1";
 const inputDir = process.argv[2];
 const outputDir = process.argv[3];
-let  results = [];
+let results = [];
 
 function readDirectory(directory) {
   return new Promise((resolve, reject) => {
@@ -14,36 +14,54 @@ function readDirectory(directory) {
       files.forEach(file => {
         results.push(file);
       });
-      if (err) reject(err);
+      if (err)
+      {
+        reject(err);
+      }
       else {
         console.log(results);
         resolve(results);
       }
+      });
     });
-  });
-}
+  }
 
-function readFilesAndConvertToXlsx(inputDir) {
-  readDirectory(inputDir).then(function (results) {
-    convertResults(results);
-  }).catch();
-}
 
-function convertResults(results) {
-  results.forEach((file) => {
-    let filePath = inputDir + file;
-    let jsonObject = require(filePath);
-    let wb = XLSX.utils.book_new();
-    let ws_data = [];
-    console.log(Object.entries(jsonObject));
+  function readFilesAndConvertToXlsx(inputDir) {
+    readDirectory(inputDir).then((results)=> {
+      convertResults(results);
+    }).catch();
+  }
+
+  function convertResults(results) {
+    results.forEach((file) => {
+      let filePath = inputDir + file;
+      let jsonObject = require(filePath);
+      let wb = XLSX.utils.book_new();
+      let whiteSheet = XLSX.utils.aoa_to_sheet(writeResults(jsonObject));
+      XLSX.utils.book_append_sheet(wb, whiteSheet, whiteSheet_name);
+      XLSX.writeFile(wb, outputDir + file.replace(".json", ".xlsx"));
+    });
+  }
+
+  function writeResults(jsonObject) {
+    let item;
+    let sheetData = [];
     Object.entries(jsonObject).forEach(([key, value]) => {
-      let item = [[key, value]];
-      ws_data = ws_data.concat(item);
+      if (Array.isArray(value)) {
+        let stringForArrayData = "[";
+        value.forEach(arrayItem => {
+          stringForArrayData = stringForArrayData.concat(",", arrayItem);
+        });
+        stringForArrayData = stringForArrayData.concat("]");
+        value = stringForArrayData;
+        item = [key, value];
+      }
+      else {
+        item = [key, value];
+      }
+      sheetData = sheetData.concat([item]);
     });
-    let ws = XLSX.utils.aoa_to_sheet(ws_data);
-    XLSX.utils.book_append_sheet(wb, ws, new_ws_name);
-    XLSX.writeFile(wb, outputDir + file.replace(".json", ".xlsx"));
-  });
-}
-
-readFilesAndConvertToXlsx(inputDir);
+    return sheetData;
+  }
+  readFilesAndConvertToXlsx(inputDir);
